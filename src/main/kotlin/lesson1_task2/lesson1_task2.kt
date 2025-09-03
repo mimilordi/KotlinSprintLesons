c88f199f52911c4738c52d22fbe2f2738aabc2bd
vaiv <mimilordi.19995@gmail.com>
2025-09-03 19:33:16 +0300

    KL-1-2 add number of staff

---
+++ src/main/kotlin/lesson1_task2/lesson1_task2.kt
@@ -0,0 +1,17 @@
+package org.example.lesson1_task2
+
+fun main () {
+
+    val ordersCount: Int = 75
+    val thanksMessage: String = "Спасибо, что доверяете нам. Благодарим вас за заказ!"
+    var numberOfStuff = 2000
+
+    println(ordersCount)
+    println(thanksMessage)
+//    println(numberOfStuff)
+
+    numberOfStuff = 1999
+
+    println(numberOfStuff)
+
+}
