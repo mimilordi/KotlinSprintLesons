5699dc18396be45629af35aa7d86189e6acbaad2
vaiv <mimilordi.19995@gmail.com>
2025-09-03 19:03:46 +0300

    KL-1-1 add order count and thanks message variables

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,6 +4,7 @@ package org.example
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
 
-
+    val ordesCount: Int = 75
+    val thanksMessage: String = "Спасибо, что доверяете нам. Благодарим вас за заказ!"
 
 }
