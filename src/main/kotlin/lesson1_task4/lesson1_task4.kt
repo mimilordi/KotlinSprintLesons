46e5822c4639f6aaa5bdae49b85aca7e1d54a50f
vaiv <mimilordi.19995@gmail.com>
2025-09-04 20:47:37 +0300

    KS-1-4 add Gagarin flight variables with types

---
+++ src/main/kotlin/lesson1_task4/lesson1_task4.kt
@@ -0,0 +1,19 @@
+package org.example.lesson1_task4
+
+fun main() {
+
+    val length: Long = 40868600000
+    val age: Byte = 27
+    val partOfDay: Double = 0.075
+    val seconds: Short = 6480
+    val partOfYear = 2.0547945205479453E-4
+    val apogee: Int = 327000
+
+    println(length)
+    println(age)
+    println(partOfDay)
+    println(seconds)
+    println(partOfYear)
+    println(apogee)
+
+}
