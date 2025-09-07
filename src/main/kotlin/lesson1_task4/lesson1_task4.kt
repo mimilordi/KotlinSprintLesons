6e4916331c2d8087e55e095d1f2e998d3b01d2d1
vaiv <mimilordi.19995@gmail.com>
2025-09-07 12:15:24 +0300

    KS-1-5 corrected remarks

--- src/main/kotlin/lesson1_task4/lesson1_task4.kt
+++ src/main/kotlin/lesson1_task4/lesson1_task4.kt
@@ -4,9 +4,9 @@ fun main() {
 
     val length: Long = 40868600000
     val age: Byte = 27
-    val partOfDay: Double = 0.075
+    val partOfDay: Float = 0.075f
     val seconds: Short = 6480
-    val partOfYear = 2.0547945205479453E-4
+    val partOfYear: Double = 2.0547945205479453E-4
     val apogee: Int = 327000
 
     println(length)
