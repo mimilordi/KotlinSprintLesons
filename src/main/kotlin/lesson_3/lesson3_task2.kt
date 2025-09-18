94c3fe0f51b200ed67bdc8071a2fd89be5eb1b34
vaiv <mimilordi.19995@gmail.com>
2025-09-18 21:20:45 +0300

    KS-3-2 change surname

---
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -0,0 +1,17 @@
+package org.example.lesson_3
+
+fun main() {
+
+    var surname = "Андреева"
+    val name = "Татьяна"
+    val middleName = "Сергеевна"
+    var age = 20
+
+    println("$surname $name $middleName, $age")
+
+    surname = "Сидорова"
+    age = 22
+
+    println("$surname $name $middleName, $age")
+
+}
