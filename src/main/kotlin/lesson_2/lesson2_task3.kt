782bf8d60f8aa04343bbb6841dec2902ae54aeca
vaiv <mimilordi.19995@gmail.com>
2025-09-18 19:41:18 +0300

    KS-2-3 fix after review part 2

--- src/main/kotlin/lesson_2/lesson2_task3.kt
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -1,14 +1,15 @@
 package org.example.lesson_2
 
 const val MINUTES_IN_HOUR = 60
-const val DEPARTURE_HOUR = 9
-const val DEPARTURE_MINUTE = 39
-const val TRAVEL_TIME_MINUTES = 457
 
 fun main() {
 
-    val departure = DEPARTURE_HOUR * MINUTES_IN_HOUR
-    val arrivalTime = TRAVEL_TIME_MINUTES + departure + DEPARTURE_MINUTE
+    val departureHour = 9
+    val departureMinute = 39
+    val travelTimeMinutes = 457
+
+    val departure = departureHour * MINUTES_IN_HOUR
+    val arrivalTime = travelTimeMinutes + departure + departureMinute
 
     val arrivalHour = arrivalTime / MINUTES_IN_HOUR
     val arrivalMinute = arrivalTime % MINUTES_IN_HOUR
