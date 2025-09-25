8d072c4b0251bc0f1caae7ca4e30c6e564d708a7
vaiv <mimilordi.19995@gmail.com>
2025-09-25 19:24:04 +0300

    KS-4-4 fix after review

--- src/main/kotlin/lesson4/lesson4_task4.kt
+++ src/main/kotlin/lesson4/lesson4_task4.kt
@@ -2,14 +2,13 @@ package org.example.lesson4
 
 fun main() {
 
-    val day = 4
+    val day = 5
 
     val armsAndPressExercises = day % 2 != 0
-    val legsAndBackExercises = !armsAndPressExercises
     val result = """
         Упражнения для рук: $armsAndPressExercises
-        Упражнения для ног: $legsAndBackExercises
-        Упражнения для спины: $legsAndBackExercises
+        Упражнения для ног: ${!armsAndPressExercises}
+        Упражнения для спины: ${!armsAndPressExercises}
         Упражнения для пресса: $armsAndPressExercises
     """.trimIndent()
 
