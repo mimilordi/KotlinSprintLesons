fd843ef951593ae0902772fcea1924e526fe4442
vaiv <mimilordi.19995@gmail.com>
2025-10-14 20:03:55 +0300

    KS-9-3 calculate ingredients for portions

---
+++ src/main/kotlin/lesson_9/lesson9_task3.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_9
+
+fun main() {
+    val quantityOnePortion = mutableListOf(2, 50, 15)
+
+    println("Сколько порций хотели бы приготовить?")
+    val portions = readln().toInt()
+
+    val quantityUserPortion = quantityOnePortion.map {
+        it * portions
+    }
+
+    println("На $portions порций вам понадобится: Яиц - ${quantityUserPortion[0]}, молока - ${quantityUserPortion[1]},  сливочного масла - ${quantityUserPortion[2]}")
+}
