6552b898b239963dbae1cf55bdf3cff53b053090
vaiv <mimilordi.19995@gmail.com>
2025-10-15 19:58:55 +0300

    KS-9-4 sort ingredients list

---
+++ src/main/kotlin/lesson_9/lesson9_task4.kt
@@ -0,0 +1,10 @@
+package org.example.lesson_9
+
+fun main() {
+    println("Введите названия 5-ти ингредиентов: ")
+    val ingredients = readln()
+
+    val ingredientsList = ingredients.split(", ").toMutableList()
+    val sortedIngredients = ingredientsList.sortedBy { it.lowercase() }
+    println(sortedIngredients.joinToString())
+}
