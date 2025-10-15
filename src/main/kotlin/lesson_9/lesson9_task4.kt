61fa7b619d505ec33a3322e9e11a2ae41af65ac3
vaiv <mimilordi.19995@gmail.com>
2025-10-15 21:19:54 +0300

    KS-9-4 fix after review

--- src/main/kotlin/lesson_9/lesson9_task4.kt
+++ src/main/kotlin/lesson_9/lesson9_task4.kt
@@ -4,7 +4,7 @@ fun main() {
     println("Введите названия 5-ти ингредиентов: ")
     val ingredients = readln()
 
-    val ingredientsList = ingredients.split(", ").toMutableList()
+    val ingredientsList = ingredients.split(", ")
     val sortedIngredients = ingredientsList.sortedBy { it.lowercase() }
     println(sortedIngredients.joinToString())
 }
