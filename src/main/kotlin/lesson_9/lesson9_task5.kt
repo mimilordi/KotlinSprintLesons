20056ce0cb7c791c228ae36970a6b45056dc7b7d
vaiv <mimilordi.19995@gmail.com>
2025-10-20 07:54:38 +0300

    KS-9-5 fix after review

--- src/main/kotlin/lesson_9/lesson9_task5.kt
+++ src/main/kotlin/lesson_9/lesson9_task5.kt
@@ -1,18 +1,14 @@
 package org.example.lesson_9
 
 fun main() {
-    val ingredientsList = mutableListOf<String>()
+    val ingredientsList = mutableSetOf<String>()
     val ingredientsQuantity = 5
 
     do {
         println("Введи название ингредиента:")
         val ingredient = readln().lowercase()
 
-        if (ingredientsList.contains(ingredient)) {
-            println("Такой игредиет уже есть!")
-        } else {
-            ingredientsList.add(ingredient)
-        }
+        if (!ingredientsList.add(ingredient)) println("Такой игредиет уже есть!")
 
     } while (ingredientsList.size != ingredientsQuantity)
 
