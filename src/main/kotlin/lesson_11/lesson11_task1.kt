c561e927af2524a18f95e9906da3c1f03e4e0dd3
vaiv <mimilordi.19995@gmail.com>
2025-10-24 15:36:59 +0300

    KS-11-1 fix after review

--- src/main/kotlin/lesson_11/lesson11_task1.kt
+++ src/main/kotlin/lesson_11/lesson11_task1.kt
@@ -1,4 +1,23 @@
-package org.example.lesson_11_task_1
+package lesson_11
+
+fun main() {
+    val firstUser = User(
+        userId = 456875,
+        login = "vally",
+        password = "robot-robot",
+        email = "bortyk32@gmail.com"
+    )
+
+    val secondUser = User(
+        userId = 654234,
+        login = "user",
+        password = "admin",
+        email = "kosmopolis@gmail.com"
+    )
+
+    firstUser.printUserInfo()
+    secondUser.printUserInfo()
+}
 
 class User(
     val userId: Int,
