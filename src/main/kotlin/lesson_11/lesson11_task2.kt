49adde6e26852691101a2dd2324a8b4858e632f2
vaiv <mimilordi.19995@gmail.com>
2025-10-24 15:39:42 +0300

    KS-11-2 fix after review

--- src/main/kotlin/lesson_11/lesson11_task2.kt
+++ src/main/kotlin/lesson_11/lesson11_task2.kt
@@ -1,4 +1,18 @@
-package org.example.lesson_11_task_2
+package lesson_11
+
+fun main() {
+    val personalInfo = User2(
+        userId = 342543,
+        login = "vivi",
+        password = "test-123",
+        email = "kolpak@gmail.com",
+    )
+
+    personalInfo.getBio()
+    personalInfo.changePassword()
+
+    personalInfo.printUserInfo()
+}
 
 class User2(
     val userId: Int,
