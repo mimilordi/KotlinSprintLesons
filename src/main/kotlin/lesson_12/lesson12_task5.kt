11ce62fed43a5f0964b81a5b63dfeafcda6e9a6e
vaiv <mimilordi.19995@gmail.com>
2025-11-13 20:25:02 +0300

    KS-12-5 fix after review

--- src/main/kotlin/lesson_12/lesson12_task5.kt
+++ src/main/kotlin/lesson_12/lesson12_task5.kt
@@ -3,20 +3,17 @@ package org.example.lesson_12
 import kotlin.random.Random
 
 fun main() {
-    var days = 30
-    val daysTemperature: MutableList<Forecast1> = mutableListOf()
-
-    while (days > 0) {
-        days--
-        val randomDayTemp = (210..350).random()
-        val randomNightTemp = (200..300).random()
-        val randomPrecision = Random.nextBoolean()
-        daysTemperature.add(Forecast1(randomDayTemp, randomNightTemp, randomPrecision))
+    val weatherList = List(30) { _ ->
+        Forecast1(
+            dayTemperature = (210..350).random(),
+            nightTemperature = (200..300).random(),
+            hasPrecipitation = Random.nextBoolean()
+        )
     }
 
-    val averageDayTemp = daysTemperature.map { it.dayTemperature }.average()
-    val averageNightTemp = daysTemperature.map { it.nightTemperature }.average()
-    val daysHasPrecision = daysTemperature.count { it.hasPrecipitation }
+    val averageDayTemp = weatherList.map { it.dayTemperature }.average()
+    val averageNightTemp = weatherList.map { it.nightTemperature }.average()
+    val daysHasPrecision = weatherList.count { it.hasPrecipitation }
 
     println("Средняя дневная температура: ${"%.1f".format(averageDayTemp)}")
     println("Средняя ночная температура: ${"%.1f".format(averageNightTemp)}")
@@ -24,10 +21,8 @@ fun main() {
 }
 
 class Forecast1(dayTemperature: Int, nightTemperature: Int, val hasPrecipitation: Boolean) {
-
     val dayTemperature = (dayTemperature - TEMP_KELVIN).toInt()
     val nightTemperature = (nightTemperature - TEMP_KELVIN).toInt()
-
 }
 
 const val TEMP_KELVIN = 273.15
